fd8ef293222a5945173452a4d5afb1126b81cbfb
Thirumal Venkat <me@thirumal.in>
2015-03-12 23:22:28 +0530

    Exploited the scope of free variables

--- sicp/sqrt-abstracted.scm
+++ sicp/sqrt-abstracted.scm
@@ -5,30 +5,30 @@
 ; Square root function, alter variable DELTA for more precision
 ; Default precision: 0.00000001
 (define (sqrt x)
-  ; HELPER for: sqrt
+  ; HELPER for: is-good-enough
   ; Delta value for square root accuracy
   ; Change this if you need more precision
   (define DELTA 0.00000001)
   ; HELPER for: sqrt 
   ; Square root recursive function
-  (define (sqrt-recursive guess x)
+  (define (sqrt-recursive guess)
     ; HELPER: Check if the guess was good enough
-    (define (is-good-enough guess x)
+    (define (is-good-enough guess)
       ; HELPER for: is-good-enough 
       ; Find the absolute value of a number
-      (define (abs x) (if (< x 0) (- x) x))
+      (define (abs a) (if (< a 0) (- a) a))
       ; HELPER for: is-good-enough 
       ; Function to find the square of a number
-      (define (square x) (* x x))
+      (define (square a) (* a a))
       ; CODE for: is-good-enough 
       ; Checks if the guess is good enough
       (< (abs (- (/ (square guess) x) 1)) DELTA))
     ; HELPER for: sqrt-recursive 
     ; Function to improve our guess
-    (define (improve-guess guess x)
+    (define (improve-guess guess)
       ; HELPER for: improve-guess
       ; Function to find the average of two numbers
-      (define (average x y) (/ (+ x y) 2))
+      (define (average a b) (/ (+ a b) 2))
       ; CODE for: improve-guess
       ; Improve our guess
       (average guess (/ x guess)))
@@ -36,12 +36,12 @@
     ; See if guess is good enough, if so then
     ; that is our square root, else refine and call
     ; this routine again to check the same recursively
-    (if (is-good-enough guess x)
+    (if (is-good-enough guess)
         guess
-        (sqrt-recursive (improve-guess guess x) x)))
+        (sqrt-recursive (improve-guess guess))))
   ; CODE for: sqrt
   ; Call the routine with an initial guess of 1
-  (sqrt-recursive 1.0 x))
+  (sqrt-recursive 1.0))
 
 ; Test the square root routine
 (sqrt 0.00000001)
