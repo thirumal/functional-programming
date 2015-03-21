84053409ccaa96923e86db2c0af8484d21a69a9c
Thirumal Venkat <me@thirumal.in>
2015-03-21 13:01:17 +0530

    Updated half-interval method

--- sicp/half-interval.rkt
+++ sicp/half-interval.rkt
@@ -7,12 +7,24 @@
 ; root present in [a,b]
 
 ; Helper functions
+(define (cube x) (* x x x))
 (define (square x) (* x x))
 (define (average x y) (/ (+ x y) 2.0))
 (define (abs x) (if (< x 0) (- x) x))
 
 ; Half interval function to find the root of f(x)
-(define (half-interval f a b tolerance)
+(define (half-interval f a b)
+  (let ((a-value (f a))
+        (b-value (f b))
+        (tolerance 0.00001))
+    (cond ((and (negative? a-value) (positive? b-value))
+           (search f a b tolerance))
+          ((and (negative? b-value) (positive? a-value))
+           (search f b a tolerance))
+          (else (error "Values are of not opposite sign" a b)))))
+
+; Modified binary search value
+(define (search f a b tolerance)  
   ; See if the guess is good enough?
   (define (good-enough? test-value)
     (< (abs test-value) tolerance))
@@ -25,5 +37,9 @@
   (iter a b (average a b)))
 
 ; Tests
-(half-interval (lambda (x) (- x 1)) 0.5 50 0.00001)
-(half-interval (lambda (x) (- (square x) 1)) 0 50 0.0001)
+; f(x) = x - 1
+(half-interval (lambda (x) (- x 1)) 0.5 2)
+; f(x) = x^2 - 1
+(half-interval (lambda (x) (- (square x) 1)) 0 5)
+; f(x) = x^3 + 3x^2 + 3x + 1
+(half-interval (lambda (x) (+ (cube x) (* 3 (square x)) (* 3 x) 1)) -2 0)
